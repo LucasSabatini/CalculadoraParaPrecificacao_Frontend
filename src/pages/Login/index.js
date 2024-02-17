b4cb6d3f73de5a62132cb4a08f05f9cc88b3ad78
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-16 22:21:48 -0300

    Reformulação da Tela de Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles.css';
 
 import calc_image from '../../assets/calc_image.jpg'
+import logoImage from '../../assets/logoImage.svg'
 
 export default function Login() {
     return (
         <div className="login-container">
             <section className="form">
+                <img src={logoImage} alt='Sabatini Logo'/>
                 <form>
                     <h1>Acesse sua Conta</h1>
                     <input placeholder='E-mail'/>
