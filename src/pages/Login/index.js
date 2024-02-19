6bdff85236be4d7635ec01989793fcc17964fce4
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-18 21:30:24 -0300

    Implementação da integração com a API

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 import './styles.css';
 
+import api from '../../services/api';
 import calc_image from '../../assets/calc_image.jpg'
 import logoImage from '../../assets/logoImage.svg'
 
 export default function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const routeChange = () => {
+        const path = '/register';
+        navigate(path);
+    };
+
+    async function login(e) {
+        e.preventDefault();
+
+        const data = {
+            email,
+            password,
+        };
+
+        try {
+            const response = await api.post('api/auth/authenticate', data);
+
+            localStorage.setItem('token', response.data.token)
+            navigate('/rawmaterials')
+        } catch (err) {
+            alert('Login falhou! Tente novamente.');
+        }
+    };
+
     return (
         <div className="login-container">
             <section className="form">
                 <img src={logoImage} alt='Sabatini Logo'/>
-                <form>
+                <form onSubmit={login}>
                     <h1>Acesse sua Conta</h1>
-                    <input placeholder='E-mail'/>
-                    <input type='password' placeholder='Senha'/>
-
-                    <button className='button' type='submit'>Login</button>
+                    <input placeholder='E-mail' value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input type='password' placeholder='Senha' value={password} onChange={e => setPassword(e.target.value)}/>
+                    <button className='button' type='submit'>Entrar</button>
                 </form>
+                
+                <h1>Não tem uma Conta?</h1>
+                <button className='button' type='submit' onClick={routeChange}>Registrar</button>
 
             </section>
 
-            <img src={calc_image} alt='Login'/>
+            <img src={calc_image} alt='Entrar'/>
 
         </div>
     )
