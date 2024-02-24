66111c0d9458a7f819c895bfa3908af45a1b2bb7
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-24 17:20:08 -0300

    Readequação da página de Cadastro de Matéria Prima, para se conectar à API e ao banco de dados

--- src/pages/NewRawMaterial/index.js
+++ src/pages/NewRawMaterial/index.js
@@ -1,31 +1,88 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { FiArrowLeft } from "react-icons/fi";
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImage from '../../assets/logoImage.svg'
 
 export default function NewRawMaterial() {
+
+    const [id, setId] = useState(null);
+    const [nameRawMaterial, setNameRawMaterial] = useState('');
+    const [pricePaid, setPricePaid] = useState('');
+    const [weightUsedInRecipe, setWeightUsedInRecipe] = useState('');
+    const [weightPurchased, setWeightPurchased] = useState('');
+    const [finalCost, setFinalCost] = useState('');
+    const [user_id, setUser_id] = useState('');
+
+    const token = localStorage.getItem('token');
+
+    const navigate = useNavigate();
+
+    async function createNewRawMaterial(e) {
+        e.preventDefault();
+
+        const data = {
+            nameRawMaterial,
+            pricePaid,
+            weightUsedInRecipe,
+            weightPurchased,
+        }
+
+        const header = {
+            Authorization: `Bearer ${token}`
+        }
+
+        try {
+            await api.post('api/materiaprima', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            navigate('/rawmaterials');
+        } catch (err) {
+            alert('Erro ao cadastrar Matéria-Prima! Tente novamente.')
+        }
+    }
+
     return (
-        <div className="newRawMaterial-container">
-            <div className="content">
-                <section className="form">
-                    <img src={logoImage} alt="Sabatini"/>
+        <div className='newRawMaterial-container'>
+            <div className='content'>
+                <section className='form'>
+                    <img src={logoImage} alt='Sabatini' />
                     <h1>Adicionar Matéria-Prima</h1>
                     <p>Insira os dados da Matéria-Prima e clique em Adicionar</p>
-                    <Link className="back-link" to="/rawmaterials">
-                        <FiArrowLeft size={16} color="#246DDA"/>
+                    <Link className='back-link' to='/rawmaterials'>
+                        <FiArrowLeft size={16} color='#246DDA' />
                         <span>Início</span>
                     </Link>
                 </section>
-                <form>
-                    <input placeholder="Nome" />
-                    <input placeholder="Preço Pago" />
-                    <input placeholder="Peso Usado" />
-                    <input placeholder="Peso Comprado" />
+                <form onSubmit={createNewRawMaterial}>
+                    <input
+                        placeholder='Nome'
+                        value={nameRawMaterial}
+                        onChange={e => setNameRawMaterial(e.target.value)}
+                    />
+                    <input
+                        placeholder='Preço Pago'
+                        value={pricePaid}
+                        onChange={e => setPricePaid(e.target.value)}
+                    />
+                    <input
+                        placeholder='Peso Usado'
+                        value={weightUsedInRecipe}
+                        onChange={e => setWeightUsedInRecipe(e.target.value)}
+                    />
+                    <input
+                        placeholder='Peso Comprado'
+                        value={weightPurchased}
+                        onChange={e => setWeightPurchased(e.target.value)}
+                    />
 
-                    <button className="button" type="submit">Adicionar</button>
+                    <button className='button' type='submit'>Adicionar</button>
                 </form>
             </div>
         </div>
