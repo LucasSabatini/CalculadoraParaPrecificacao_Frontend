894732f8909250432353df64d63081fe3cd5680c
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-27 19:00:30 -0300

    Funções Deletar Matéria Prima e Logout adicionadas

--- src/pages/RawMaterials/index.js
+++ src/pages/RawMaterials/index.js
@@ -5,7 +5,6 @@ import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
 import api from '../../services/api';
 
 import './styles.css';
-
 import logoImage from '../../assets/logoImage.svg'
 
 export default function RawMaterials() {
@@ -16,6 +15,24 @@ export default function RawMaterials() {
     const firstName = localStorage.getItem('firstName');
 
     const navigate = useNavigate();
+    
+    async function logout() {
+        localStorage.clear();
+        navigate('/');
+    }
+    async function deleteRawMaterial(id) {
+        try {
+            await api.delete(`api/materiaprima/${id}`, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
+
+            setRawMaterials(rawMaterials.filter(rawMaterial => rawMaterial.id !== id))
+        } catch (err) {
+            alert("Falha ao excluir! Tente novamente.");
+        }
+    }
 
     useEffect(() => {
         api.get('api/materiaprima', {
@@ -30,32 +47,32 @@ export default function RawMaterials() {
     return (
         <div className="rawMaterial-container">
             <header>
-                <img src={logoImage} alt="Sabatini"/>
+                <img src={logoImage} alt="Sabatini" />
                 <span>Bem Vindo, <strong>{firstName.toUpperCase()}</strong>!</span>
                 <Link className="button" to="/rawmaterials/new">Adicionar Nova Matéria-Prima</Link>
-                <button type="button">
-                    <FiPower size={18} color="#246DDA"/>
+                <button onClick={logout} type="button">
+                    <FiPower size={18} color="#246DDA" />
                 </button>
             </header>
 
             <h1>Matérias-Primas Cadastradas</h1>
             <ul>
                 {rawMaterials.map(rawMaterial => (
-                    <li>
+                    <li key={rawMaterial.id}>
                         <strong>Nome:</strong>
                         <p>{rawMaterial.nameRawMaterial}</p>
                         <strong>Preço Pago:</strong>
-                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(rawMaterial.pricePaid)}</p>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(rawMaterial.pricePaid)}</p>
                         <strong>Peso Usado:</strong>
                         <p>{rawMaterial.weightUsedInRecipe}g</p>
                         <strong>Peso Comprado:</strong>
                         <p>{rawMaterial.weightPurchased}g</p>
                         <strong>Custo Final:</strong>
-                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(rawMaterial.finalCost)}</p>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(rawMaterial.finalCost)}</p>
                         <button type="button">
                             <FiEdit size={20} color="#246DDA" />
                         </button>
-                        <button type="button">
+                        <button onClick={() => deleteRawMaterial(rawMaterial.id)} type="button">
                             <FiTrash2 size={20} color="#246DDA" />
                         </button>
                     </li>
