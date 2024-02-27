4c308ca1ef86982e23a96a649151db4d4208f906
lucassabatini <lucas.frsabatini@gmail.com>
2024-02-27 15:01:29 -0300

    Implementação da função de listagem com acesso ao Banco de Dados e retirada dos mocks

--- src/pages/RawMaterials/index.js
+++ src/pages/RawMaterials/index.js
@@ -1,17 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FiPower, FiEdit, FiTrash2 } from "react-icons/fi";
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImage from '../../assets/logoImage.svg'
 
 export default function RawMaterials() {
+
+    const [rawMaterials, setRawMaterials] = useState([]);
+
+    const accessToken = localStorage.getItem('accessToken');
+    const firstName = localStorage.getItem('firstName');
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        api.get('api/materiaprima', {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        }).then(response => {
+            setRawMaterials(response.data)
+        })
+    });
+
     return (
         <div className="rawMaterial-container">
             <header>
                 <img src={logoImage} alt="Sabatini"/>
-                <span>Bem Vindo, <strong>Lucas</strong>!</span>
+                <span>Bem Vindo, <strong>{firstName.toUpperCase()}</strong>!</span>
                 <Link className="button" to="/rawmaterials/new">Adicionar Nova Matéria-Prima</Link>
                 <button type="button">
                     <FiPower size={18} color="#246DDA"/>
@@ -20,86 +40,26 @@ export default function RawMaterials() {
 
             <h1>Matérias-Primas Cadastradas</h1>
             <ul>
-                <li>
-                    <strong>Nome:</strong>
-                    <p>Óxido de Zinco</p>
-                    <strong>Preço Pago:</strong>
-                    <p>R$250,00</p>
-                    <strong>Peso Usado:</strong>
-                    <p>250g</p>
-                    <strong>Peso Comprado:</strong>
-                    <p>1000g</p>
-                    <strong>Custo Final:</strong>
-                    <p>R$62,50</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#246DDA"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#246DDA"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Nome:</strong>
-                    <p>Óxido de Zinco</p>
-                    <strong>Preço Pago:</strong>
-                    <p>R$250,00</p>
-                    <strong>Peso Usado:</strong>
-                    <p>250g</p>
-                    <strong>Peso Comprado:</strong>
-                    <p>1000g</p>
-                    <strong>Custo Final:</strong>
-                    <p>R$62,50</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#246DDA"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#246DDA"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Nome:</strong>
-                    <p>Óxido de Zinco</p>
-                    <strong>Preço Pago:</strong>
-                    <p>R$250,00</p>
-                    <strong>Peso Usado:</strong>
-                    <p>250g</p>
-                    <strong>Peso Comprado:</strong>
-                    <p>1000g</p>
-                    <strong>Custo Final:</strong>
-                    <p>R$62,50</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#246DDA"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#246DDA"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>Nome:</strong>
-                    <p>Óxido de Zinco</p>
-                    <strong>Preço Pago:</strong>
-                    <p>R$250,00</p>
-                    <strong>Peso Usado:</strong>
-                    <p>250g</p>
-                    <strong>Peso Comprado:</strong>
-                    <p>1000g</p>
-                    <strong>Custo Final:</strong>
-                    <p>R$62,50</p>
-
-                    <button type="button">
-                        <FiEdit size={20} color="#246DDA"/>
-                    </button>
-
-                    <button type="button">
-                        <FiTrash2 size={20} color="#246DDA"/>
-                    </button>
-                </li>
+                {rawMaterials.map(rawMaterial => (
+                    <li>
+                        <strong>Nome:</strong>
+                        <p>{rawMaterial.nameRawMaterial}</p>
+                        <strong>Preço Pago:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(rawMaterial.pricePaid)}</p>
+                        <strong>Peso Usado:</strong>
+                        <p>{rawMaterial.weightUsedInRecipe}g</p>
+                        <strong>Peso Comprado:</strong>
+                        <p>{rawMaterial.weightPurchased}g</p>
+                        <strong>Custo Final:</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(rawMaterial.finalCost)}</p>
+                        <button type="button">
+                            <FiEdit size={20} color="#246DDA" />
+                        </button>
+                        <button type="button">
+                            <FiTrash2 size={20} color="#246DDA" />
+                        </button>
+                    </li>
+                ))}
             </ul>
         </div>
     );
